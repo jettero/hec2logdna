9e1afba0a858935e51b7551c1848a53941df7f7d
Paul Miller <paul@jettero.pl>
2021-10-09 00:07:55 -0400

    hubble tweaks

--- min.py
+++ min.py
@@ -14,14 +14,15 @@ app.logger.setLevel(10)
 
 HTTP = urllib3.PoolManager()
 HOSTNAME = socket.getfqdn()
-ENDPOINT = f"https://logs.logdna.com/logs/ingest?hostname={HOSTNAME}"
+ENDPOINT = f"https://logs.logdna.com/logs/ingest?"
 MAX_CONTENT_BYTES = 1e5
-MAX_READ = 100000
+MAX_READ = 500000
 
 
 @app.route("/services/collector/event", methods=["POST", "GET"])
 def hec():
     now = int(time.time())
+    host = HOSTNAME
     dec = json.JSONDecoder()
     data = request.stream.read(MAX_READ).decode()
     events = list()
@@ -34,11 +35,17 @@ def hec():
             for n, nn in (("sourcetype", "app"), ("event", "line"), ("time", "timestamp"), ("fields", "meta")):
                 if n in j:
                     line[nn] = j.pop(n)
-            try:
-                d = json.dumps(line["line"])
-                line["line"] = d
-            except json.JSONEncodeError:
-                pass
+            if isinstance(line["line"], (list, tuple, dict)):
+                e = line["line"]
+                line["line"] = json.dumps(e)
+                for item in ("host", "dest_host", "dest_fqdn"):
+                    if item in e:
+                        host = e["host"]
+                        break
+                if "loggername" in e:
+                    line["app"] = e["loggername"]
+                if "log_level" in e:
+                    line["level"] = e["log_level"]
             events.append(line)
             data = data[r:].lstrip()
         except json.JSONDecodeError as e:
@@ -54,7 +61,7 @@ def hec():
     if not events:
         return Response("please give me event data. I want the event data.", status=300)
     dat = json.dumps({"lines": events})
-    url = f"{ENDPOINT}&now={now}"
+    url = f"{ENDPOINT}hostname={host}&now={now}"
     app.logger.info("events=%d payload=%d url=%s", len(events), len(dat), url)
     res = HTTP.request("POST", url, body=dat, headers=headers)
     # app.logger.info("logdna res=%d %s", res.status, res.data)
