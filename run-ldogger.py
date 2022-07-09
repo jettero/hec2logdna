1ae70ab3b7937cfa7a85b16069b1a9a008b0f4dc
Paul Miller <paul@jettero.pl>
2022-07-09 12:39:26 -0400

    split that last one

--- run-ldogger.py
+++ run-ldogger.py
@@ -8,5 +8,11 @@ sys.path.insert(0, ".")
 import ldogger.cmd
 
 if __name__ == "__main__":
-    name = os.path.basename(sys.argv[0]).replace("-linux-x86_64", "").replace("run-", "").replace(".py", "")
+    name = (
+        os.path.basename(sys.argv[0])
+        .replace("-linux", "")
+        .replace("-x86_64", "")
+        .replace("run-", "")
+        .replace(".py", "")
+    )
     getattr(ldogger.cmd, name)()
